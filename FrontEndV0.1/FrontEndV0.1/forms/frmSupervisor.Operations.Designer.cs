11ef00af95625d48d0514b4f95c4e9b2f96b4cfc
SteveCaruana <st3v3n.95@gmail.com>
2015-04-20 15:32:30 +1000

    Supervisor stuff

---
+++ FrontEndV0.1/FrontEndV0.1/forms/frmSupervisor.Operations.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace FrontEndV0._1.forms
+{
+    partial class frmSupervisor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmSupervisor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 730);
+            this.Name = "frmSupervisor";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "frmSupervisor";
+            this.Load += new System.EventHandler(this.frmSupervisor_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
