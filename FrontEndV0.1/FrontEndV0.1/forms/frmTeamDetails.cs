a0ea98680914827bc05fec6f1b5727b44ce24c2b
SteveCaruana <st3v3n.95@gmail.com>
2015-05-11 16:50:37 +1000

    Small design changes to frmTeamDetails
    After this I won't change this form anymore.

--- FrontEndV0.1/FrontEndV0.1/forms/frmTeamDetails.cs
+++ FrontEndV0.1/FrontEndV0.1/forms/frmTeamDetails.cs
@@ -7,16 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
 
 namespace FrontEndV0._1.forms
 {
     public partial class frmTeamDetails : Form
     {
+        private OracleConnection connection;
+        private Connection conn = new Connection("s7663285", "123");
+
         public frmTeamDetails(bool editable)
         {
             InitializeComponent();
-
+            connection = conn.oraConn();
         }
+
+        
 
     }
 }
