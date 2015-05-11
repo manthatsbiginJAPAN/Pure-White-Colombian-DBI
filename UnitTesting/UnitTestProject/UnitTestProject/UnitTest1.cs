eaf0dfd456c37bad94d9edb35483d0b9badd53cf
AeronStorm <sam@hartwig.com.au>
2015-05-11 15:22:32 +1000

    Unit Test Project
    created

---
+++ UnitTesting/UnitTestProject/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FrontEndV0;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestEmpDetails()
+        {
+        }
+    }
+}
