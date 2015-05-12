b9427954c67e786a7a8c9bd4b36ffdbdc793ec33
manthatsbiginJAPAN <7663285@student.swin.edu.au>
2015-05-13 00:43:39 +1000

    Assessment creation/Deletion
    Kinda works.. can someone help with the more specific stuff?

--- FrontEndV0.1/FrontEndV0.1/forms/frmAssessment.cs
+++ FrontEndV0.1/FrontEndV0.1/forms/frmAssessment.cs
@@ -16,6 +16,7 @@ namespace FrontEndV0._1.forms
         private OracleConnection connection;
         private Connection conn = new Connection("s7663285", "123");
         private DataSet ds;
+        private DataSet unitoffs;
 
         public frmAssessment(bool editable)
         {
@@ -26,6 +27,10 @@ namespace FrontEndV0._1.forms
             //Set up current assessments in form
             getAssessments();
             populateAssGrid();
+            
+            //Fill in Comboboxes
+            getUnitOfferings();
+            populateUnitOfferings();
         }
 
         private void getAssessments()
@@ -59,6 +64,147 @@ namespace FrontEndV0._1.forms
             {
                 object[] items = ds.Tables[0].Rows[i].ItemArray;
                 grdAssessmentInfo.Rows.Add(new object[] {items[0], items[3], items[4] ,items[5]} );
+            }
+        }
+
+        private void getUnitOfferings()
+        {
+            //Oracle Command to populate the dataset
+            OracleCommand cmd = new OracleCommand("UC1_22_View_Unit_Offering", connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.Add("unitoffcursor", OracleDbType.RefCursor);
+            cmd.Parameters["unitoffcursor"].Direction = ParameterDirection.ReturnValue;
+
+            connection.Open();
+            OracleDataAdapter da = new OracleDataAdapter(cmd);
+            cmd.ExecuteNonQuery();
+
+            unitoffs = new DataSet();
+
+            da.Fill(unitoffs, "unitoffcursor", (OracleRefCursor)(cmd.Parameters["unitoffcursor"].Value));
+
+            connection.Close();
+        }
+
+        private void populateUnitOfferings()
+        {
+            //Clear the grid
+            cmbUnitId.Items.Clear();
+            cmbSem.Items.Clear();
+            cmbYear.Items.Clear();
+
+            //Populate the grid from the dataset
+            int rowcnt = unitoffs.Tables["unitoffcursor"].Rows.Count;
+            object unitid = new object();
+            object sem = new object();
+            object year = new object();
+
+            for (int i = 0; i <= rowcnt - 1; i++)
+            {
+                unitid = unitoffs.Tables["unitoffcursor"].Rows[i][0].ToString();
+                sem = unitoffs.Tables["unitoffcursor"].Rows[i][1].ToString();
+                year = unitoffs.Tables["unitoffcursor"].Rows[i][2].ToString();
+
+                if (!cmbUnitId.Items.Contains(unitid.ToString()))
+                    cmbUnitId.Items.Add(unitid.ToString());
+                if (!cmbSem.Items.Contains(sem))
+                    cmbSem.Items.Add(sem);
+                if (!cmbYear.Items.Contains(year))
+                    cmbYear.Items.Add(year);
+            }
+        }
+
+        private void cmbUnitId_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            if (cmbUnitId.SelectedIndex != -1)
+                cmbSem.Enabled = true;
+
+            int rowcnt = unitoffs.Tables["unitoffcursor"].Rows.Count;
+            object sem = new object();
+
+            {
+
+                for (int i = 0; i <= rowcnt - 1; i++)
+                {
+                    sem = unitoffs.Tables["unitoffcursor"].Rows[i][1].ToString();
+
+                    if (!cmbSem.Items.Contains(sem))
+                        cmbSem.Items.Add(sem);
+                }
+            }
+        }
+
+        private void cmbSem_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            if (cmbSem.SelectedIndex != -1)
+                cmbYear.Enabled = true;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            OracleCommand cmd = new OracleCommand("UC2_9_REGISTER_ASSESSMENT", connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.Add("ASSid", txtAssID.Text);
+            cmd.Parameters.Add("ASSTitle", txtAssTitle.Text);
+            cmd.Parameters.Add("ASSdesc", txtAssDesc.Text);
+            cmd.Parameters.Add("unitID", cmbUnitId.SelectedItem.ToString());
+            cmd.Parameters.Add("semester", cmbSem.SelectedItem.ToString());
+            cmd.Parameters.Add("year", cmbYear.SelectedItem.ToString());
+            cmd.Parameters.Add("marking guide", txtMarkingGuide.Text);
+
+            connection.Open();
+            cmd.ExecuteNonQuery();
+            connection.Close();
+
+            getAssessments();
+            populateAssGrid();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int selectedrow = grdAssessmentInfo.SelectedCells[0].RowIndex;
+            OracleCommand cmd = new OracleCommand("UC2_12_DELETE_ASSESSMENT", connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.Add("assid", grdAssessmentInfo.Rows[selectedrow].Cells[0].Value.ToString() );
+            cmd.Parameters.Add("unitid", grdAssessmentInfo.Rows[selectedrow].Cells[1].Value.ToString());
+            cmd.Parameters.Add("sem", grdAssessmentInfo.Rows[selectedrow].Cells[2].Value);
+            cmd.Parameters.Add("year", grdAssessmentInfo.Rows[selectedrow].Cells[3].Value);
+
+            connection.Open();
+            cmd.ExecuteNonQuery();
+            connection.Close();
+
+            MessageBox.Show("Entry Deleted");
+
+            getAssessments();
+            populateAssGrid();
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (btnDelete.Text == "Save")
+            {
+                OracleCommand cmd = new OracleCommand("UC2_10_UPDATE_ASSESSMENT", connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add("ASSid", txtAssID.Text);
+                cmd.Parameters.Add("ASSTitle", txtAssTitle.Text);
+                cmd.Parameters.Add("ASSdesc", txtAssDesc.Text);
+                cmd.Parameters.Add("unitID", cmbUnitId.SelectedItem.ToString());
+                cmd.Parameters.Add("semester", cmbSem.SelectedItem.ToString());
+                cmd.Parameters.Add("year", cmbYear.SelectedItem.ToString());
+                cmd.Parameters.Add("marking guide", txtMarkingGuide.Text);
+
+                connection.Open();
+                cmd.ExecuteNonQuery();
+                connection.Close();
+            }
+            else
+            {
+                
             }
         }
     }
