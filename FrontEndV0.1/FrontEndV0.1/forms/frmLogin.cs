e4e8c9038dd42b82aa724f9882e32421b3340862
MusicMaestroMan <MusicMaestroMan@users.noreply.github.com>
2015-05-13 22:32:45 +1000

    Updated frmLogin
    Working on adding login accessibility...

--- FrontEndV0.1/FrontEndV0.1/forms/frmLogin.cs
+++ FrontEndV0.1/FrontEndV0.1/forms/frmLogin.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -6,18 +6,29 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
 
 namespace FrontEndV0._1.forms
 {
     public partial class frmLogin : Form
     {
+        private OracleConnection connection;
+        private Connection conn = new Connection("s7663285", "123");
+
         private frmEmpDashboard frmEmpDashboard;
         private frmStuDashboard frmStuDashboard;
+        private DataSet employees;
+        private DataSet students;
         private string User;
 
         public frmLogin()
         {
             InitializeComponent();
+            connection = conn.oraConn();
+
+            //Form load, without yet instantiating subforms
+            frmStuDashboard = null;
             frmEmpDashboard = null;
             User = null;
         }
@@ -27,12 +38,13 @@ namespace FrontEndV0._1.forms
             //Code here needs to be redone at some point to actually log people in
             //Will include logic to work out usertype
 
+            User = txtUsername.Text; //record the user
+            char UserLetter = Convert.ToChar(User.Substring(0, 1));
+            //MessageBox.Show("User is " + User + " with letter '" + UserLetter + "'");
+
             if (frmEmpDashboard == null)
             {
-                User = txtUsername.Text; //record the user
-                char UserLetter = Convert.ToChar(User.Substring(0, 1));
-
-                //will eventually validate user based on db
+                //will validate user based on db
 
                 //check if a student
                 if (UserLetter == 's') //since student ids would start with an 's' like s748932
@@ -42,49 +54,109 @@ namespace FrontEndV0._1.forms
                         frmStuDashboard = new frmStuDashboard(User);
                         frmStuDashboard.FormClosing += frmStuDashboardClosing;
                         frmStuDashboard.Show();
-                    } 
+                        return;
+                    }
                     else
                     {
                         //load in all student rows
+                        getStudents();
+                        int rowcnt = students.Tables[0].Rows.Count;
+
                         //search dataset by stuID (username), returning a row if it exists
+                        for (int i = 0; i <= rowcnt - 1; i++)
+                        {
+                            //search for username
+                            if (User == students.Tables[0].Rows[i][0].ToString())
+                            {
+                                //check if password matches before confirming login
+                                if (txtPassword.Text == students.Tables[0].Rows[i][5].ToString())
+                                {
+                                    frmStuDashboard = new frmStuDashboard(User);
+                                    frmStuDashboard.FormClosing += frmStuDashboardClosing;
+                                    frmStuDashboard.Show();
+                                    return;
+                                }
+                                MessageBox.Show("Incorrect Password, please try again");
+                            }
+                            MessageBox.Show("Student Login not recognised, please try again");
+                        }
+                    }
+
+
+                    //check if an employee
+                    if (UserLetter == 'e')
+                    {
+                        //load in all employee rows
+                        //search dataset by empID (username), returning a row if it exists
                         //check if password matches before confirming login
                     }
-                }
 
+                    //(for ease of access...)
+                    if (User.ToLower() == "admin" || User.ToLower() == "convenor" || User.ToLower() == "supervisor")
+                    {
+                        frmEmpDashboard = new frmEmpDashboard(User);
+                        frmEmpDashboard.FormClosing += frmEmpDashboardClosing;
+                        frmEmpDashboard.Show();
 
-                //check if an employee
-                if (UserLetter == 'e')
-                {
-                    //load in all employee rows
-                    //search dataset by empID (username), returning a row if it exists
-                    //check if password matches before confirming login
-                }
+                        //old code:
+                        /*if (User.ToLower() == "admin")
+                            frmEmpDashboard = new frmEmpDashboard("admin");
 
-                //(for ease of access...)
-                if (User.ToLower() == "admin" || User.ToLower() == "convenor" || User.ToLower() == "supervisor")
-                {
-                    frmEmpDashboard = new frmEmpDashboard(User);                  
-                    frmEmpDashboard.FormClosing += frmEmpDashboardClosing;
-                    frmEmpDashboard.Show();
+                        if (User.ToLower() == "supervisor")
+                            frmEmpDashboard = new frmEmpDashboard("supervisor");
 
-                    //old code:
-                    /*if (User.ToLower() == "admin")
-                        frmEmpDashboard = new frmEmpDashboard("admin");
-
-                    if (User.ToLower() == "supervisor")
-                        frmEmpDashboard = new frmEmpDashboard("supervisor");
-
-                    if (User.ToLower() == "convenor")
-                        frmEmpDashboard = new frmEmpDashboard("convenor");
-                    */
-                }
-                else
-                {
-                    MessageBox.Show("Please enter a valid username and password", "Invalid", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                    txtPassword.Focus();
-                    return;
+                        if (User.ToLower() == "convenor")
+                            frmEmpDashboard = new frmEmpDashboard("convenor");
+                        */
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please enter a valid username and password", "Invalid", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        txtPassword.Focus();
+                        return;
+                    }
                 }
             }
+        }
+
+        private void getStudents()
+        {
+            //Oracle Command to populate the dataset
+            OracleCommand cmd = new OracleCommand("UC1_11_View_Student", connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.Add("stucursor", OracleDbType.RefCursor);
+            cmd.Parameters["stucursor"].Direction = ParameterDirection.ReturnValue;
+
+            connection.Open();
+            OracleDataAdapter da = new OracleDataAdapter(cmd);
+            cmd.ExecuteNonQuery();
+
+            students = new DataSet();
+
+            da.Fill(students, "stucursor", (OracleRefCursor)(cmd.Parameters["stucursor"].Value));
+
+            connection.Close();
+        }
+
+        private void getEmployees()
+        {
+            //Oracle Command to populate the dataset
+            OracleCommand cmd = new OracleCommand("UC1_3_View_Employee", connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.Add("empcursor", OracleDbType.RefCursor);
+            cmd.Parameters["empcursor"].Direction = ParameterDirection.ReturnValue;
+
+            connection.Open();
+            OracleDataAdapter da = new OracleDataAdapter(cmd);
+            cmd.ExecuteNonQuery();
+
+            employees = new DataSet();
+
+            da.Fill(employees, "empcursor", (OracleRefCursor)(cmd.Parameters["empcursor"].Value));
+
+            connection.Close();
         }
 
         private void frmEmpDashboardClosing(object sender, FormClosingEventArgs e)
