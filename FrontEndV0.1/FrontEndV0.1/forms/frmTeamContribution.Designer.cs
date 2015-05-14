8fc1c2c035b420ffca3a6c7dd9fbe54571d5a334
Nathan <the.music.maestro.man@gmail.com>
2015-05-14 14:18:19 +1000

    Trying to restore...
    Cos github is having major issues...
    
    Problem seems to be with the frmTeamContribution as the form won't
    display, and the designer is full of "head <<<<<" blocks. Even when they
    are removed though it has a ton of errors where it can't recognise the
    objects that the designer refers to... So I've saved a copy of the
    former code (both cs file and the designer file, with all the
    additions), but will reload the old stable version of the form.
    
    Have no idea wth is going on with the suo and cache files though...

--- FrontEndV0.1/FrontEndV0.1/forms/frmTeamContribution.Designer.cs
+++ FrontEndV0.1/FrontEndV0.1/forms/frmTeamContribution.Designer.cs
@@ -29,15 +29,8 @@
         private void InitializeComponent()
         {
             this.lblTitle = new System.Windows.Forms.Label();
-<<<<<<< HEAD
-            this.grdTaskInfo = new System.Windows.Forms.DataGridView();
-            this.colTaskId = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.colTaskDesc = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Duedate = new System.Windows.Forms.DataGridViewTextBoxColumn();
-=======
             this.grdAssessmentInfo = new System.Windows.Forms.DataGridView();
             this.cmbYear = new System.Windows.Forms.ComboBox();
->>>>>>> origin/master
             this.btnAdd = new System.Windows.Forms.Button();
             this.btnEdit = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
@@ -54,10 +47,6 @@
             this.txtTaskID = new System.Windows.Forms.TextBox();
             this.lblTaskID = new System.Windows.Forms.Label();
             this.gbDetails = new System.Windows.Forms.GroupBox();
-            this.txtTaskId = new System.Windows.Forms.TextBox();
-            this.lblTaskId = new System.Windows.Forms.Label();
-            this.lblDueDate = new System.Windows.Forms.Label();
-            this.dtDue = new System.Windows.Forms.DateTimePicker();
             this.txtTaskDesc = new System.Windows.Forms.TextBox();
             this.lblTaskDesc = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.grdAssessmentInfo)).BeginInit();
@@ -84,28 +73,6 @@
             this.grdAssessmentInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.grdAssessmentInfo.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.colTaskId,
-<<<<<<< HEAD
-            this.colTaskDesc,
-            this.Duedate});
-            this.grdTaskInfo.Location = new System.Drawing.Point(648, 80);
-            this.grdTaskInfo.Margin = new System.Windows.Forms.Padding(4);
-            this.grdTaskInfo.Name = "grdTaskInfo";
-            this.grdTaskInfo.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.grdTaskInfo.Size = new System.Drawing.Size(561, 677);
-            this.grdTaskInfo.TabIndex = 26;
-            // 
-            // colTaskId
-            // 
-            this.colTaskId.FillWeight = 40.60914F;
-            this.colTaskId.HeaderText = "Task Id";
-            this.colTaskId.Name = "colTaskId";
-            // 
-            // colTaskDesc
-            // 
-            this.colTaskDesc.FillWeight = 159.3909F;
-            this.colTaskDesc.HeaderText = "Task Description";
-            this.colTaskDesc.Name = "colTaskDesc";
-=======
             this.colTaskDesc});
             this.grdAssessmentInfo.Location = new System.Drawing.Point(486, 65);
             this.grdAssessmentInfo.Name = "grdAssessmentInfo";
@@ -128,39 +95,21 @@
             this.cmbYear.Name = "cmbYear";
             this.cmbYear.Size = new System.Drawing.Size(121, 21);
             this.cmbYear.TabIndex = 48;
->>>>>>> origin/master
-            // 
-            // Duedate
-            // 
-            this.Duedate.HeaderText = "Due Date";
-            this.Duedate.Name = "Duedate";
-            this.Duedate.ReadOnly = true;
             // 
             // btnAdd
             // 
             this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-<<<<<<< HEAD
-            this.btnAdd.Location = new System.Drawing.Point(30, 711);
-            this.btnAdd.Margin = new System.Windows.Forms.Padding(4);
-=======
             this.btnAdd.Location = new System.Drawing.Point(342, 65);
->>>>>>> origin/master
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(137, 37);
             this.btnAdd.TabIndex = 20;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
-            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnEdit
             // 
             this.btnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-<<<<<<< HEAD
-            this.btnEdit.Location = new System.Drawing.Point(241, 711);
-            this.btnEdit.Margin = new System.Windows.Forms.Padding(4);
-=======
             this.btnEdit.Location = new System.Drawing.Point(342, 110);
->>>>>>> origin/master
             this.btnEdit.Name = "btnEdit";
             this.btnEdit.Size = new System.Drawing.Size(137, 37);
             this.btnEdit.TabIndex = 23;
@@ -170,12 +119,7 @@
             // btnDelete
             // 
             this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-<<<<<<< HEAD
-            this.btnDelete.Location = new System.Drawing.Point(441, 711);
-            this.btnDelete.Margin = new System.Windows.Forms.Padding(4);
-=======
             this.btnDelete.Location = new System.Drawing.Point(342, 153);
->>>>>>> origin/master
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(137, 37);
             this.btnDelete.TabIndex = 25;
@@ -302,139 +246,34 @@
             // 
             // gbDetails
             // 
-            this.gbDetails.Controls.Add(this.txtTaskId);
-            this.gbDetails.Controls.Add(this.lblTaskId);
-            this.gbDetails.Controls.Add(this.lblDueDate);
-            this.gbDetails.Controls.Add(this.dtDue);
             this.gbDetails.Controls.Add(this.txtTaskDesc);
             this.gbDetails.Controls.Add(this.lblTaskDesc);
             this.gbDetails.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-<<<<<<< HEAD
-            this.gbDetails.Location = new System.Drawing.Point(3, 414);
-            this.gbDetails.Margin = new System.Windows.Forms.Padding(4);
-            this.gbDetails.Name = "gbDetails";
-            this.gbDetails.Padding = new System.Windows.Forms.Padding(4);
-            this.gbDetails.Size = new System.Drawing.Size(629, 254);
-=======
             this.gbDetails.Location = new System.Drawing.Point(8, 271);
             this.gbDetails.Name = "gbDetails";
             this.gbDetails.Size = new System.Drawing.Size(472, 337);
->>>>>>> origin/master
             this.gbDetails.TabIndex = 27;
             this.gbDetails.TabStop = false;
             this.gbDetails.Text = "Details";
             // 
-            // txtTaskId
-            // 
-            this.txtTaskId.Location = new System.Drawing.Point(165, 20);
-            this.txtTaskId.Name = "txtTaskId";
-            this.txtTaskId.Size = new System.Drawing.Size(212, 23);
-            this.txtTaskId.TabIndex = 54;
-            // 
-            // lblTaskId
-            // 
-            this.lblTaskId.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTaskId.Location = new System.Drawing.Point(22, 20);
-            this.lblTaskId.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblTaskId.Name = "lblTaskId";
-            this.lblTaskId.Size = new System.Drawing.Size(143, 57);
-            this.lblTaskId.TabIndex = 53;
-            this.lblTaskId.Text = "Task ID:";
-            // 
-            // lblDueDate
-            // 
-            this.lblDueDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDueDate.Location = new System.Drawing.Point(8, 219);
-            this.lblDueDate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblDueDate.Name = "lblDueDate";
-            this.lblDueDate.Size = new System.Drawing.Size(143, 57);
-            this.lblDueDate.TabIndex = 52;
-            this.lblDueDate.Text = "Date Due:";
-            // 
-            // dtDue
-            // 
-            this.dtDue.Location = new System.Drawing.Point(165, 220);
-            this.dtDue.Name = "dtDue";
-            this.dtDue.Size = new System.Drawing.Size(256, 23);
-            this.dtDue.TabIndex = 51;
-            // 
             // txtTaskDesc
             // 
             this.txtTaskDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-<<<<<<< HEAD
-            this.txtTaskDesc.Location = new System.Drawing.Point(165, 101);
-            this.txtTaskDesc.Margin = new System.Windows.Forms.Padding(4);
-            this.txtTaskDesc.Multiline = true;
-            this.txtTaskDesc.Name = "txtTaskDesc";
-            this.txtTaskDesc.Size = new System.Drawing.Size(456, 112);
-=======
             this.txtTaskDesc.Location = new System.Drawing.Point(123, 39);
             this.txtTaskDesc.Multiline = true;
             this.txtTaskDesc.Name = "txtTaskDesc";
             this.txtTaskDesc.Size = new System.Drawing.Size(343, 173);
->>>>>>> origin/master
             this.txtTaskDesc.TabIndex = 50;
             // 
             // lblTaskDesc
             // 
             this.lblTaskDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-<<<<<<< HEAD
-            this.lblTaskDesc.Location = new System.Drawing.Point(14, 105);
-            this.lblTaskDesc.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-=======
             this.lblTaskDesc.Location = new System.Drawing.Point(10, 42);
->>>>>>> origin/master
             this.lblTaskDesc.Name = "lblTaskDesc";
             this.lblTaskDesc.Size = new System.Drawing.Size(107, 46);
             this.lblTaskDesc.TabIndex = 48;
             this.lblTaskDesc.Text = "Task Description:";
             // 
-<<<<<<< HEAD
-            // grdAssessmentsInfo
-            // 
-            this.grdAssessmentsInfo.AllowUserToAddRows = false;
-            this.grdAssessmentsInfo.AllowUserToDeleteRows = false;
-            this.grdAssessmentsInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.grdAssessmentsInfo.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.AssID,
-            this.UnitID,
-            this.Year,
-            this.Semester});
-            this.grdAssessmentsInfo.Location = new System.Drawing.Point(30, 80);
-            this.grdAssessmentsInfo.MultiSelect = false;
-            this.grdAssessmentsInfo.Name = "grdAssessmentsInfo";
-            this.grdAssessmentsInfo.RowTemplate.Height = 24;
-            this.grdAssessmentsInfo.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.grdAssessmentsInfo.Size = new System.Drawing.Size(602, 327);
-            this.grdAssessmentsInfo.TabIndex = 28;
-            this.grdAssessmentsInfo.SelectionChanged += new System.EventHandler(this.grdAssessmentsInfo_SelectionChanged);
-            // 
-            // AssID
-            // 
-            this.AssID.HeaderText = "AssID";
-            this.AssID.Name = "AssID";
-            this.AssID.ReadOnly = true;
-            // 
-            // UnitID
-            // 
-            this.UnitID.HeaderText = "UnitID";
-            this.UnitID.Name = "UnitID";
-            this.UnitID.ReadOnly = true;
-            // 
-            // Year
-            // 
-            this.Year.HeaderText = "Year";
-            this.Year.Name = "Year";
-            this.Year.ReadOnly = true;
-            // 
-            // Semester
-            // 
-            this.Semester.HeaderText = "Semester";
-            this.Semester.Name = "Semester";
-            this.Semester.ReadOnly = true;
-            // 
-=======
->>>>>>> origin/master
             // frmTeamContribution
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -446,11 +285,7 @@
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.btnEdit);
             this.Controls.Add(this.btnDelete);
-<<<<<<< HEAD
-            this.Margin = new System.Windows.Forms.Padding(4);
-=======
             this.Controls.Add(this.gbIdentifyingInformation);
->>>>>>> origin/master
             this.Name = "frmTeamContribution";
             ((System.ComponentModel.ISupportInitialize)(this.grdAssessmentInfo)).EndInit();
             this.gbIdentifyingInformation.ResumeLayout(false);
@@ -485,18 +320,5 @@
         public System.Windows.Forms.GroupBox gbDetails;
         public System.Windows.Forms.TextBox txtTaskDesc;
         public System.Windows.Forms.Label lblTaskDesc;
-<<<<<<< HEAD
-        private System.Windows.Forms.DataGridView grdAssessmentsInfo;
-        private System.Windows.Forms.DataGridViewTextBoxColumn AssID;
-        private System.Windows.Forms.DataGridViewTextBoxColumn UnitID;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Year;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Semester;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Duedate;
-        private System.Windows.Forms.DateTimePicker dtDue;
-        public System.Windows.Forms.Label lblDueDate;
-        private System.Windows.Forms.TextBox txtTaskId;
-        public System.Windows.Forms.Label lblTaskId;
-=======
->>>>>>> origin/master
     }
 }
--- FrontEndV0.1/FrontEndV0.1/forms/frmTeamContribution.cs
+++ FrontEndV0.1/FrontEndV0.1/forms/frmTeamContribution.cs
@@ -6,88 +6,14 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-using Oracle.DataAccess.Client;
-using Oracle.DataAccess.Types;
 
 namespace FrontEndV0._1.forms
 {
     public partial class frmTeamContribution : Form
     {
-
-        private OracleConnection connection;
-        private Connection conn = new Connection("s7663285", "123");
-
-        private DataSet tasks;
-
-        private frmAssessment _parent;
-
-        public frmTeamContribution(frmAssessment parent)
+        public frmTeamContribution()
         {
             InitializeComponent();
-
-            connection = conn.oraConn();
-
-            _parent = parent;
-
-            _parent.populateAssGrid(this.grdAssessmentsInfo);
         }
-
-        private void btnAdd_Click(object sender, EventArgs e)
-        {
-            OracleCommand cmd = new OracleCommand("UC2_21_REGISTER_ASSTASK", connection);
-            cmd.CommandType = CommandType.StoredProcedure;
-
-            cmd.Parameters.Add("Task ID", txtTaskId.Text);
-            cmd.Parameters.Add("AssID", grdAssessmentsInfo.Rows[grdAssessmentsInfo.SelectedRows[0].Index].Cells[0].Value.ToString());
-            cmd.Parameters.Add("UnitId", grdAssessmentsInfo.Rows[grdAssessmentsInfo.SelectedRows[0].Index].Cells[1].Value.ToString());
-            cmd.Parameters.Add("Semseter", grdAssessmentsInfo.Rows[grdAssessmentsInfo.SelectedRows[0].Index].Cells[2].Value.ToString());
-            cmd.Parameters.Add("Year", grdAssessmentsInfo.Rows[grdAssessmentsInfo.SelectedRows[0].Index].Cells[3].Value.ToString());
-            cmd.Parameters.Add("Task Desc", txtTaskDesc.Text);
-            cmd.Parameters.Add("Due Date", dtDue.Value.ToString("dd/MMM/yyyy"));
-            //
-            
-
-            connection.Open();
-            cmd.ExecuteNonQuery();
-            connection.Close();
-        }
-
-        private void grdAssessmentsInfo_SelectionChanged(object sender, EventArgs e)
-        {
-            displayTasks(grdAssessmentsInfo.Rows[grdAssessmentsInfo.SelectedRows[0].Index].Cells[0].Value.ToString());
-        }
-
-        private void displayTasks(string assid)
-        {
-            //Doesn't quite work but it's close... im tired pls do it for me lol
-
-            grdTaskInfo.Rows.Clear();
-
-            OracleCommand cmd = new OracleCommand("UC2_23_VIEW_ASSTASK", connection);
-            cmd.CommandType = CommandType.StoredProcedure;
-
-            cmd.Parameters.Add("assid", assid);
-            cmd.Parameters.Add("taskcursor", OracleDbType.RefCursor);
-            cmd.Parameters["taskcursor"].Direction = ParameterDirection.ReturnValue;
-
-            connection.Open();
-            OracleDataAdapter da = new OracleDataAdapter(cmd);
-            cmd.ExecuteNonQuery();
-
-            tasks = new DataSet();
-
-            da.Fill(tasks, "taskcursor", (OracleRefCursor)(cmd.Parameters["taskcursor"].Value) );
-
-            connection.Close();
-
-            int rowcnt = tasks.Tables["taskcursor"].Rows.Count;
-
-            for (int i = 0; i <= rowcnt - 1; i++)
-            {
-                object[] items = tasks.Tables[0].Rows[i].ItemArray;
-                grdTaskInfo.Rows.Add(new object[] { items[0], items[5], items[6]});
-            } 
-        }
-
     }
 }
