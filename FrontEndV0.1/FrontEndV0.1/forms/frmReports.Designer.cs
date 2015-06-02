ebb4e54db1e4342f70df9f292bdd387442cffb79
SteveCaruana <st3v3n.95@gmail.com>
2015-06-03 01:28:52 +1000

    syncing to get changes

--- FrontEndV0.1/FrontEndV0.1/forms/frmReports.Designer.cs
+++ FrontEndV0.1/FrontEndV0.1/forms/frmReports.Designer.cs
@@ -35,6 +35,12 @@
             this.btnProjectReport = new System.Windows.Forms.Button();
             this.btnSupervisorReport = new System.Windows.Forms.Button();
             this.btnEnrolmentReport = new System.Windows.Forms.Button();
+            this.cbYear = new System.Windows.Forms.ComboBox();
+            this.cbSemester = new System.Windows.Forms.ComboBox();
+            this.cbUnitID = new System.Windows.Forms.ComboBox();
+            this.lblSem = new System.Windows.Forms.Label();
+            this.lblYear = new System.Windows.Forms.Label();
+            this.lblUnitID = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnConUnitReport
@@ -70,7 +76,7 @@
             // btnTeamDetails
             // 
             this.btnTeamDetails.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnTeamDetails.Location = new System.Drawing.Point(21, 429);
+            this.btnTeamDetails.Location = new System.Drawing.Point(21, 298);
             this.btnTeamDetails.Name = "btnTeamDetails";
             this.btnTeamDetails.Size = new System.Drawing.Size(146, 57);
             this.btnTeamDetails.TabIndex = 43;
@@ -80,7 +86,7 @@
             // btnProjectReport
             // 
             this.btnProjectReport.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnProjectReport.Location = new System.Drawing.Point(21, 336);
+            this.btnProjectReport.Location = new System.Drawing.Point(21, 216);
             this.btnProjectReport.Name = "btnProjectReport";
             this.btnProjectReport.Size = new System.Drawing.Size(146, 57);
             this.btnProjectReport.TabIndex = 44;
@@ -90,7 +96,7 @@
             // btnSupervisorReport
             // 
             this.btnSupervisorReport.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSupervisorReport.Location = new System.Drawing.Point(21, 249);
+            this.btnSupervisorReport.Location = new System.Drawing.Point(21, 153);
             this.btnSupervisorReport.Name = "btnSupervisorReport";
             this.btnSupervisorReport.Size = new System.Drawing.Size(146, 57);
             this.btnSupervisorReport.TabIndex = 45;
@@ -100,18 +106,81 @@
             // btnEnrolmentReport
             // 
             this.btnEnrolmentReport.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEnrolmentReport.Location = new System.Drawing.Point(21, 165);
+            this.btnEnrolmentReport.Location = new System.Drawing.Point(21, 566);
             this.btnEnrolmentReport.Name = "btnEnrolmentReport";
             this.btnEnrolmentReport.Size = new System.Drawing.Size(146, 57);
             this.btnEnrolmentReport.TabIndex = 46;
-            this.btnEnrolmentReport.Text = "Enrolments in a Unit";
+            this.btnEnrolmentReport.Text = "See Enrolments";
             this.btnEnrolmentReport.UseVisualStyleBackColor = true;
+            this.btnEnrolmentReport.Click += new System.EventHandler(this.btnEnrolmentReport_Click);
+            // 
+            // cbYear
+            // 
+            this.cbYear.Enabled = false;
+            this.cbYear.FormattingEnabled = true;
+            this.cbYear.Location = new System.Drawing.Point(21, 528);
+            this.cbYear.Name = "cbYear";
+            this.cbYear.Size = new System.Drawing.Size(121, 21);
+            this.cbYear.TabIndex = 52;
+            // 
+            // cbSemester
+            // 
+            this.cbSemester.Enabled = false;
+            this.cbSemester.FormattingEnabled = true;
+            this.cbSemester.Location = new System.Drawing.Point(21, 480);
+            this.cbSemester.Name = "cbSemester";
+            this.cbSemester.Size = new System.Drawing.Size(121, 21);
+            this.cbSemester.TabIndex = 51;
+            // 
+            // cbUnitID
+            // 
+            this.cbUnitID.FormattingEnabled = true;
+            this.cbUnitID.Location = new System.Drawing.Point(21, 432);
+            this.cbUnitID.Name = "cbUnitID";
+            this.cbUnitID.Size = new System.Drawing.Size(121, 21);
+            this.cbUnitID.TabIndex = 50;
+            // 
+            // lblSem
+            // 
+            this.lblSem.AutoSize = true;
+            this.lblSem.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSem.Location = new System.Drawing.Point(17, 456);
+            this.lblSem.Name = "lblSem";
+            this.lblSem.Size = new System.Drawing.Size(91, 21);
+            this.lblSem.TabIndex = 48;
+            this.lblSem.Text = "Semester:";
+            // 
+            // lblYear
+            // 
+            this.lblYear.AutoSize = true;
+            this.lblYear.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblYear.Location = new System.Drawing.Point(17, 504);
+            this.lblYear.Name = "lblYear";
+            this.lblYear.Size = new System.Drawing.Size(57, 21);
+            this.lblYear.TabIndex = 49;
+            this.lblYear.Text = "Year:";
+            // 
+            // lblUnitID
+            // 
+            this.lblUnitID.AutoSize = true;
+            this.lblUnitID.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUnitID.Location = new System.Drawing.Point(17, 408);
+            this.lblUnitID.Name = "lblUnitID";
+            this.lblUnitID.Size = new System.Drawing.Size(84, 21);
+            this.lblUnitID.TabIndex = 47;
+            this.lblUnitID.Text = "Unit ID:";
             // 
             // frmReports
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(914, 646);
+            this.Controls.Add(this.cbYear);
+            this.Controls.Add(this.cbSemester);
+            this.Controls.Add(this.cbUnitID);
+            this.Controls.Add(this.lblSem);
+            this.Controls.Add(this.lblYear);
+            this.Controls.Add(this.lblUnitID);
             this.Controls.Add(this.btnEnrolmentReport);
             this.Controls.Add(this.btnSupervisorReport);
             this.Controls.Add(this.btnProjectReport);
@@ -134,6 +203,12 @@
         public System.Windows.Forms.Button btnProjectReport;
         public System.Windows.Forms.Button btnSupervisorReport;
         public System.Windows.Forms.Button btnEnrolmentReport;
+        private System.Windows.Forms.ComboBox cbYear;
+        private System.Windows.Forms.ComboBox cbSemester;
+        private System.Windows.Forms.ComboBox cbUnitID;
+        public System.Windows.Forms.Label lblSem;
+        public System.Windows.Forms.Label lblYear;
+        public System.Windows.Forms.Label lblUnitID;
 
     }
 }
--- FrontEndV0.1/FrontEndV0.1/forms/frmReports.cs
+++ FrontEndV0.1/FrontEndV0.1/forms/frmReports.cs
@@ -20,6 +20,7 @@ namespace FrontEndV0._1.forms
         private Connection conn = new Connection("s7663285", "123");
         private DataSet units;
         private DataSet unitoffs;
+        private DataSet unitoffs2;
         private string User;
         private bool IsAdmin;
 
@@ -56,15 +57,14 @@ namespace FrontEndV0._1.forms
             connection.Close();
         }
 
-        private void getUnitOfferings(string unitid)
+        private void getUnitOfferings()
         {
-          //Oracle Command to populate the dataset
-            OracleCommand cmd = new OracleCommand("UC4_1_View_Unit_Offering", connection);
+            //Oracle Command to populate the dataset
+            OracleCommand cmd = new OracleCommand("UC1_22_View_Unit_Offering", connection);
             cmd.CommandType = CommandType.StoredProcedure;
 
             cmd.Parameters.Add("unitoffcursor", OracleDbType.RefCursor);
             cmd.Parameters["unitoffcursor"].Direction = ParameterDirection.ReturnValue;
-            cmd.Parameters.Add("unit", unitid);
             cmd.Parameters.Add("user", User);
             string role;
             if (IsAdmin)
@@ -81,49 +81,100 @@ namespace FrontEndV0._1.forms
 
             da.Fill(unitoffs, "unitoffcursor", (OracleRefCursor)(cmd.Parameters["unitoffcursor"].Value));
 
+            connection.Close();
+        }
+
+        private void getThisUnitOfferings(string unitid)
+        {
+          //Oracle Command to populate the dataset
+            OracleCommand cmd = new OracleCommand("UC4_1_View_Unit_Offering", connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.Add("unitoffcursor2", OracleDbType.RefCursor);
+            cmd.Parameters["unitoffcursor2"].Direction = ParameterDirection.ReturnValue;
+            cmd.Parameters.Add("unit", unitid);
+            cmd.Parameters.Add("user", User);
+            string role;
+            if (IsAdmin)
+                role = "admin";
+            else
+                role = "convenor";
+            cmd.Parameters.Add("role", role);
+
+            connection.Open();
+            OracleDataAdapter da = new OracleDataAdapter(cmd);
+            cmd.ExecuteNonQuery();
+
+            unitoffs2 = new DataSet();
+
+            da.Fill(unitoffs2, "unitoffcursor2", (OracleRefCursor)(cmd.Parameters["unitoffcursor2"].Value));
+
             connection.Close(); 
         }
 
-       
+        private void populatecbUnitID()
+        {
+            //Clear boxes
+            cbUnitID.Items.Clear();
+
+            //Populate the boxes from the dataset
+            int rowcnt = unitoffs.Tables["unitoffcursor"].Rows.Count;
+            object unitid = new object();
+
+
+            for (int i = 0; i <= rowcnt - 1; i++)
+            {
+                unitid = unitoffs.Tables[0].Rows[i][0].ToString();
+                cbUnitID.Items.Add(unitid.ToString());
+            }
+
+        }
 
         private void btnConUnitReport_Click(object sender, EventArgs e)
         {
             txtReportDisplay.Clear();
-
             getUnits();
 
-            txtReportDisplay.Text = "******** List of Units: *******";
+            txtReportDisplay.Text = "******** List of Units and Unit Offerings: *********" + Environment.NewLine;
 
-            int rowcnt = units.Tables["unitcursor"].Rows.Count;
+            int unitrowcnt = units.Tables["unitcursor"].Rows.Count;
             string unitid = "";
             string unitname = "";
 
-            for (int i = 0; i <= rowcnt - 1; i++)
+            for (int i = 0; i < unitrowcnt; i++)
             {
                 unitid = units.Tables["unitcursor"].Rows[i][0].ToString();
                 unitname = units.Tables["unitcursor"].Rows[i][1].ToString();
 
                 //populate textbox
                 txtReportDisplay.Text += System.Environment.NewLine + unitid + " " + unitname;
+                getThisUnitOfferings(unitid);
 
-                for (int j = 0; j <= rowcnt - j; j++)
+                int offrowcnt = unitoffs2.Tables["unitoffcursor"].Rows.Count;
+                for (int j = 0; j < offrowcnt; j++)
                 {
-                    getUnitOfferings(unitid);
-
-                    int rowcnt2 = unitoffs.Tables["unitoffcursor"].Rows.Count;
                     string sem = "";
                     string year = "";
                     string confirstname = "";
                     string conlastname = "";
 
-                    sem = unitoffs.Tables[0].Rows[j][0].ToString();
-                    year = unitoffs.Tables[0].Rows[j][1].ToString();
-                    confirstname = unitoffs.Tables[0].Rows[j][2].ToString();
-                    conlastname = unitoffs.Tables[0].Rows[j][3].ToString();
+                    sem = unitoffs2.Tables[0].Rows[j][0].ToString();
+                    year = unitoffs2.Tables[0].Rows[j][1].ToString();
+                    confirstname = unitoffs2.Tables[0].Rows[j][2].ToString();
+                    conlastname = unitoffs2.Tables[0].Rows[j][3].ToString();
 
                     txtReportDisplay.Text += System.Environment.NewLine + "     -" + sem + " " + year + " " + confirstname + " " + conlastname;
                 }
             }
+            txtReportDisplay.Text += Environment.NewLine + Environment.NewLine;
+            txtReportDisplay.Text += "******************* End of List ********************";
+        }
+
+       
+
+        private void btnEnrolmentReport_Click(object sender, EventArgs e)
+        {
+
         }
 
     }
